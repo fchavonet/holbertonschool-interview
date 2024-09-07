8e4bc23e5d7b7055d9ce1c9f8b566c5f118342ad
fchavonet <fchavonet@mailo.com>
2024-09-07 15:16:14 +0200

    Removing `{}` and addingt a comment on `sandpiles.h` file.

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -13,9 +13,7 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
-		{
 			grid1[i][j] += grid2[i][j];
-		}
 	}
 
 	/* Stabilize the grid */
@@ -88,9 +86,7 @@ void toppling_round(int grid[3][3])
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
-		{
 			grid[i][j] += temp_grid[i][j];
-		}
 	}
 }
 
--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+/* Function prototypes */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 int is_stable(int grid[3][3]);
 void toppling_round(int grid[3][3]);
