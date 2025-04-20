c0a66db6986d337727d4071f097aac318681d1de
fchavonet <fchavonet@mail.fr>
2025-04-20 15:55:54 +0200

    Creating a function that sorts an array of integers in ascending order using the Radix sort algorithm.

---
+++ radix_sort/sort.h
@@ -0,0 +1,22 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_array - Prints an array of integers
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size);
+
+/**
+ * radix_sort - Sorts an array of integers in ascending order
+ *               using the LSD radix sort algorithm
+ * @array: The array to sort
+ * @size: Number of elements in @array
+ */
+void radix_sort(int *array, size_t size);
+
+#endif /* SORT_H */
